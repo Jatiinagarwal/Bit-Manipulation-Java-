3b8e8f563101f61ade56b9917bc81a35085c1b50
Jatin Aggarwal <121922356+Jatiinagarwal@users.noreply.github.com>
2023-06-23 15:18:38 +0530

    Add files via upload

---
+++ bit_manipulation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class bit_manipulation {
+    public static void main(String args[]) {
+        System.out.println(5&6);//&& is for comparision(and operator)
+        System.out.println(5|6);//|=pipe(or operator)
+        System.out.println(5^6);//XOR operator
+        System.out.println(~5);//1's complement
+        System.out.println(5<<2);//left shift
+        System.out.println(6>>1);//right shift
+    }
+} 
